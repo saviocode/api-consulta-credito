2f86205f40c5c1a74620965aeeaf5323de3fe584
savio nascimento <savio.nascimento.gomes@gmail.com>
2025-05-09 02:13:32 -0300

    Implement controller(criação dos endpoints)

---
+++ src/main/java/com/gestionna/apiconsultacredito/controller/CreditoController.java
@@ -0,0 +1,26 @@
+package com.gestionna.apiconsultacredito.controller;
+
+import com.gestionna.apiconsultacredito.entity.dto.CreditoDto;
+import com.gestionna.apiconsultacredito.service.CreditoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/creditos")
+@RequiredArgsConstructor
+public class CreditoController {
+    private final CreditoService servico;
+
+    @GetMapping("/{numeroNfse}")
+    public ResponseEntity<List<CreditoDto>> getPorNfse(@PathVariable String numeroNfse) {
+        return ResponseEntity.ok(servico.buscarPorNfse(numeroNfse));
+    }
+
+    @GetMapping("/credito/{numeroCredito}")
+    public ResponseEntity<CreditoDto> getPorNumero(@PathVariable String numeroCredito) {
+        return ResponseEntity.ok(servico.buscarPorNumeroCredito(numeroCredito));
+    }
+}

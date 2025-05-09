6cc44abd6190db19376e9a628b1a58dae491ea8f
savio nascimento <savio.nascimento.gomes@gmail.com>
2025-05-09 00:55:52 -0300

    Initial commit: esqueleto do projeto

---
+++ src/main/java/com/gestionna/apiconsultacredito/ApiConsultaCreditoApplication.java
@@ -0,0 +1,13 @@
+package com.gestionna.apiconsultacredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsultaCreditoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiConsultaCreditoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestionna/apiconsultacredito/ApiConsultaCreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionna.apiconsultacredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConsultaCreditoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
